2932cc487d821ad77bb0073cee1194ca21d2e99c
Arindam Singh <118199077+SinghArindam@users.noreply.github.com>
2025-02-09 20:25:31 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Rotate Array by One/rotate-array-by-one.cpp
@@ -0,0 +1,53 @@
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+    void rotate(vector<int> &arr) {
+        int n = arr.size();
+        // storing the last element of the array in a variable x
+        int x = arr[n - 1], i;
+
+        // shifting each element one position to the right
+        for (i = n - 1; i > 0; i--)
+            arr[i] = arr[i - 1];
+
+        // placing the stored element x in the first position of the array
+        arr[0] = x;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    cin.ignore();
+    while (t--) {
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        Solution ob;
+        ob.rotate(arr);
+        for (long long i = 0; i < arr.size(); i++) {
+            cout << arr[i] << " ";
+        }
+        cout << endl;
+        cout << "~" << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
